6936f021094bf1aa96bf3c171b82add63d92c86c
Mike Ludemann <mike-ludemann@hotmail.de>
2021-01-19 17:36:02 +0100

    Setup complete - vue-twitter

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # vue-twitter
+
 Vue Twitter Component
+
+## Properties
+
+### Twitter
+
+|Key|Value|Type|
+|:--|:----|:---|
+|twitterid|The ID of the Twitter contact|String|
+|style|Styleguide of this element - ["grid", "timeline"]|String|
+|timeline|Set the timeline|String|
---
+++ src/components/twitter.vue
@@ -0,0 +1,45 @@
+<template lang="html">
+	<a id="tweets"> Tweets by {{this.twitterid}} </a>
+</template>
+
+<script lang="js">
+	export default {
+		name: 'twitter',
+		props: {
+			twitterid: String,
+			format: String
+		},
+		mounted() {
+			this.insertExternalSource();
+		},
+		data() {
+			return {
+
+			}
+		},
+		methods: {
+			insertExternalSource: function () {
+
+				var js = document.createElement("script");
+				js.setAttribute("src", "https://platform.twitter.com/widgets.js");
+				js.setAttribute("async", "true");
+				js.setAttribute("charset", "utf-8");
+				document.getElementsByTagName("head").item(0).appendChild(js);
+
+				var tweet = document.getElementById("tweets");
+				tweet.setAttribute("href", "http://twitter.com/" + this.twitterid);
+				tweet.setAttribute("class", "twitter-" + this.format);
+
+			}
+		},
+		computed: {
+
+		}
+}
+</script>
+
+<style scoped lang="scss">
+	.twitter {
+
+	}
+</style>
